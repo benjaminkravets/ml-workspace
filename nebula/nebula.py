d72e73a2b16dcc4c7b9ca67fa436c7826c4d577b
Benjamin Kravets <bakravets@gmail.com>
2023-04-19 21:56:50 -0400

    tuning

--- nebula/nebula.py
+++ nebula/nebula.py
@@ -126,8 +126,8 @@ model = build_model(
     head_size=256,
     num_heads=4,
     ff_dim=4,
-    num_transformer_blocks=1,
-    mlp_units=[128],
+    num_transformer_blocks=2,
+    mlp_units=[64],
     mlp_dropout=0.4,
     dropout=0.25,
 )
