d43289e08df23f7e0fce474f68abecc49fe9e9e3
Benjamin Kravets <bakravets@gmail.com>
2023-08-09 13:27:37 -0400

    sarima grid search

--- arima.py
+++ arima.py
@@ -2,70 +2,59 @@
 from pandas import read_csv
 from matplotlib import pyplot
 from statsmodels.tsa.arima.model import ARIMA, ARIMAResults
+from statsmodels.tsa.statespace.sarimax import SARIMAX
 from sklearn.metrics import mean_squared_error
 from math import sqrt
 from pandas import DataFrame
+import sys
 
+series = read_csv('humidityhourdiff.csv', header=0, usecols=[1]).values[0:40000]
 
-series = read_csv('spydailydiff.csv', header=0, usecols=[1])
-
+look_back = 2
 # fit model
-model = ARIMA(series, order=(5,1,0))
+model = SARIMAX(series[0:int(len(series) * 1)], order=(3,0,0))
 model_fit = model.fit()
-
-if(False):
-	# summary of fit model
-	print(model_fit.summary())
-	# line plot of residuals
-	residuals = DataFrame(model_fit.resid)
-	residuals.plot()
-	pyplot.show()
-	# density plot of residuals
-	residuals.plot(kind='kde')
-	pyplot.show()
-	# summary stats of residuals
-	print(residuals.describe())
 
 
 # split into train and test sets
+""" 
 X = series.values
 size = int(len(X) * 0.66)
 train, test = X[0:size], X[size:len(X)]
 history = [x for x in train]
 predictions = list()
+
+# walk-forward validation
+print(type(series), type(history))
+sys.exit()
+ """
 mass = 1
 masshistory = []
-# walk-forward validation
-for t in range(len(test)):
+for t in range(len(series)-10):
 	#model = ARIMA(history, order=(5,1,0))
 	#model_fit = model.fit()
 	#output = model_fit.forecast()
-	yhat = float(model_fit.predict(start=t+4,end=t+4))
+	#print(output)
+	output = model_fit.predict(start=t, end=t+2)
+	yhat = output[0]
+	obs = series[t]
 
-	predictions.append(yhat)
-	obs = test[t]
-	history.append(obs)
-	print('predicted=%f, expected=%f' % (yhat, obs))
-
+	#print(series[t-3:t])
+	#print(obs, output)
+	#print()
 
 	tol = .00000
 
-	if (yhat > 0 + tol):
+	if yhat > 0 + tol:
 		mass *= (1 + obs) * (1-.000)
-	if (yhat < 0 - tol):
+	if yhat < 0 - tol:
 		mass *= (1 / (1 + obs) * (1-.000))
-	
+
 	masshistory.append(float(mass))
 
-# evaluate forecasts
-rmse = sqrt(mean_squared_error(test, predictions))
-print('Test RMSE: %.3f' % rmse)
-# plot forecasts against actual outcomes
-""" 
-pyplot.plot(test)
-pyplot.plot(predictions, color='red')
-pyplot.show()
- """
 
+	#print('predicted=%f, expected=%f' % (yhat, obs))
+# evaluate forecasts
+print(mass)
 pyplot.plot(masshistory)
 pyplot.show()
