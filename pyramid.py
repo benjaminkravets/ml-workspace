0aca63556ede8ec107a6304c0916e144e05638eb
Benjamin Kravets <bakravets@gmail.com>
2023-08-14 21:01:06 -0400

    backup, finish pmdarima demo setup

--- pyramid.py
+++ pyramid.py
@@ -1,43 +1,45 @@
-import numpy as np
-import pandas as pd
+import numpy as np 
+import pandas as pd 
 import matplotlib.pyplot as plt
-# %matplotlib inline
-
-import pmdarima as pm
-from pmdarima.datasets.stocks import load_msft
-
-from pmdarima.arima import ndiffs
-
-from pmdarima.model_selection import train_test_split
-from pandas import read_csv
 import sys
 
-y = read_csv('datashop/spydaily.csv', header=0, usecols=[1]).values
+import pmdarima as pm
+from pandas import read_csv
+from pmdarima.datasets.stocks import load_msft
 
-y_train = y[0:500]
-y_test = y[0:1000]
+df = load_msft()
+df.head()
 
-plt.plot(y_train)
-plt.plot(y_test)
-plt.show()
-sys.exit()
+train_len = int(df.shape[0] * 0.8)
+train_data, test_data = df[:train_len], df[train_len:]
+
+y_train = train_data['Open'].values
+y_test = test_data['Open'].values
+
+series = read_csv('datashop/humiditydaily.csv', header=0, usecols=[1]).values
+y_train = series[0:3000]
+y_test = series[3000:6000]
+
+
+print(f"{train_len} train samples")
+print(f"{df.shape[0] - train_len} test samples")
+
+from pmdarima.arima import ndiffs
 
 kpss_diffs = ndiffs(y_train, alpha=0.05, test='kpss', max_d=6)
 adf_diffs = ndiffs(y_train, alpha=0.05, test='adf', max_d=6)
 n_diffs = max(adf_diffs, kpss_diffs)
 
 print(f"Estimated differencing term: {n_diffs}")
-# Estimated differencing term: 1
 
 auto = pm.auto_arima(y_train, d=n_diffs, seasonal=False, stepwise=True,
                      suppress_warnings=True, error_action="ignore", max_p=6,
                      max_order=None, trace=True)
 
-
 from sklearn.metrics import mean_squared_error
 from pmdarima.metrics import smape
 
-model = auto  # seeded from the model we've already fit
+model = auto
 
 def forecast_one_step():
     fc, conf_int = model.predict(n_periods=1, return_conf_int=True)
@@ -48,29 +50,29 @@ def forecast_one_step():
 forecasts = []
 confidence_intervals = []
 
-for new_ob in y_test:
+z = 0
+mass = 1
+for i,new_ob in enumerate(y_test):
     fc, conf = forecast_one_step()
     forecasts.append(fc)
     confidence_intervals.append(conf)
-    print("test")
+    #print(z)
+    z += 1
+    if(1):
+        if fc > y_test[i]:
+            mass *= y_test[i+1] / y_test[i]
+        elif fc < y_test[i]:
+            mass *= y_test[i] / y_test[i+1]
+        print(mass)
+
+    if(0):
+        if fc > 0:
+            mass *= (1 + y_test[i+1])
+        elif fc < 0:
+            mass *= 1 / (1 + y_test[i+1])
+        print(mass)
     # Updates the existing model with a small number of MLE steps
     model.update(new_ob)
-
+    
 print(f"Mean squared error: {mean_squared_error(y_test, forecasts)}")
 print(f"SMAPE: {smape(y_test, forecasts)}")
-
-fig, axes = plt.subplots(2, 1, figsize=(12, 12))
-
-
-# --------------------- Actual vs. Predicted --------------------------
-axes[0].plot(y_train, color='blue', label='Training Data')
-axes[0].plot(test_data.index, forecasts, color='green', marker='o',
-             label='Predicted Price')
-
-axes[0].plot(test_data.index, y_test, color='red', label='Actual Price')
-axes[0].set_title('Microsoft Prices Prediction')
-axes[0].set_xlabel('Dates')
-axes[0].set_ylabel('Prices')
-
-axes[0].set_xticks(np.arange(0, 7982, 1300).tolist(), df['Date'][0:7982:1300].tolist())
-axes[0].legend()
