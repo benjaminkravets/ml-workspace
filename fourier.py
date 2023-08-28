1b6fff94bcef8efecd8dd0fe9fcd9218f5ac297d
Benjamin Kravets <bakravets@gmail.com>
2023-08-28 16:06:27 -0400

    change phase function from cmath to np

--- fourier.py
+++ fourier.py
@@ -1,102 +1,84 @@
-# Import the required packages
-import numpy as np
-from scipy.fft import fft, rfft
-from scipy.fft import fftfreq, rfftfreq
-import plotly.graph_objs as go
-from plotly.subplots import make_subplots
 import matplotlib.pyplot as plt
-#https://towardsdatascience.com/fourier-transform-the-practical-python-implementation-acdd32f1b96a
-class Signal:
-  """
-  Generate sinusoidal signals with specific ampltiudes, frequencies, duration,
-  sampling rate, and phase.
+import numpy as np
+import matplotlib.pyplot as plt
 
-  Example:
-    signal = Signal(amplitude=10, sampling_rate=2000.0)
-    sine = signal.sine()
-    cosine = signal.cosine()
-  """
+x = np.arange(0, 10, 0.01)
+x2 = np.arange(0, 20, 0.02)
+sin1 = np.sin(x)
+sin2 = np.sin(x2)
+x2 /= 2
+sin3 = sin1 + sin2
+plt.figure(1)
+plt.plot(x, sin1, color="blue")
+plt.plot(x, sin2, color="red")
+plt.plot(x, sin3, color="purple")
+plt.draw()
 
-  def __init__(self, amplitude=1, frequency=10, duration=1, sampling_rate=100.0, phase=0):
-    """
-    Initialize the Signal class.
 
-    Args:
-        amplitude (float): The amplitude of the signal
-        frequency (int): The frequency of the signal Hz
-        duration (float): The duration of the signal in second
-        sampling_rate (float): The sampling per second of the signal
-        phase (float): The phase of the signal in radians
-    
-    Additional parameters,which are required to generate the signal, are
-    calculated and defined to be initialized here too:
-        time_step (float): 1.0/sampling_rate
-        time_axis (np.array): Generate the time axis from the duration and
-                              the time_step of the signal. The time axis is
-                              for better representation of the signal.
-    """
-    self.amplitude = amplitude
-    self.frequency = frequency
-    self.duration = duration
-    self.sampling_rate = sampling_rate
-    self.phase = phase
-    self.time_step = 1.0/self.sampling_rate
-    self.time_axis = np.arange(0, self.duration, self.time_step)
-  
-  # Generate sine wave
-  def sine(self):
-    """
-    Method of Signal
+def decompose_fft(data: list, threshold: float = 0.0):
+    plt.figure(2)
+    print(len(data))
+    fft3 = np.fft.fft(data)
+    print(len(fft3))
+    x = np.arange(0, 10, 10 / len(data))
+    freqs = np.fft.fftfreq(len(x), 0.01)
+    recomb = np.zeros((len(x),))
+    for i in range(len(fft3)):
+        if abs(fft3[i]) / len(x) > threshold:
+            sinewave = (
+                1
+                / len(x)
+                * (
+                    fft3[i].real * np.cos(freqs[i] * 2 * np.pi * x)
+                    - fft3[i].imag * np.sin(freqs[i] * 2 * np.pi * x)
+                )
+            )
+            recomb += sinewave
+            plt.plot(x, sinewave)
+    plt.draw()
+    plt.figure(3)
+    plt.plot(x, recomb, x, data)
+    plt.show()
 
-    Returns:
-        np.array of sine wave using the pre-defined variables (amplitude,
-        frequency, time_axis, and phase)
-    """
-    return self.amplitude*np.sin(2*np.pi*self.frequency*self.time_axis+self.phase)
-  
-  # Generate cosine wave
-  def cosine(self):
-    """
-    Method of Signal
 
-    Returns:
-        np.array of cosine wave using the pre-defined variables (amplitude,
-        frequency, time_axis, and phase)
-    """
-    return self.amplitude*np.cos(2*np.pi*self.frequency*self.time_axis+self.phase)
-  
-# Generate the three signals using Signal class and its method sine()
-signal_1hz = Signal(amplitude=3, frequency=1, sampling_rate=200, duration=2)
-sine_1hz = signal_1hz.sine()
-signal_20hz = Signal(amplitude=1, frequency=20, sampling_rate=200, duration=2)
-sine_20hz = signal_20hz.sine()
-signal_10hz = Signal(amplitude=0.5, frequency=10, sampling_rate=200, duration=2)
-sine_10hz = signal_10hz.sine()
+# decompose_fft(sin3, threshold=0)
 
-# Sum the three signals to output the signal we want to analyze
-signal = sine_1hz + sine_20hz + sine_10hz
 
-# Plot the signal
-plt.plot(signal_1hz.time_axis, signal, 'b')
-plt.xlabel('Time [sec]')
-plt.ylabel('Amplitude')
-plt.title('Sum of three signals')
-plt.show(block=True)
+plt.figure(2)
+import cmath
+import math
 
-# Apply the FFT on the signal
-fourier = fft(signal)
-print(fourier)
-# Plot the result (the spectrum |Xk|)
-plt.plot(np.abs(fourier))
-plt.show()
+fft3 = np.fft.fft(sin3)
+print(type(sin3))
 
-# Calculate N/2 to normalize the FFT output
-N = len(signal)
-normalize = N/2
+""" 
+for l in range(5):
+    print(fft3[l])
+    #print(cmath.phase(fft3[l]))
+    print(np.angle(fft3[l]))
+    print()
+ """
+freqs = np.fft.fftfreq(len(x), 0.01)
+threshold = 0.0
+recomb = np.zeros((len(x),))
+middle = len(x) // 2 + 1
+for i in range(middle):
+    if abs(fft3[i]) / (len(x)) > threshold:
+        if i == 0:
+            coeff = 2
+        else:
+            coeff = 1
+        sinusoid = (
+            1
+            / (len(x) * coeff / 2)
+            * (abs(fft3[i]) * np.cos(freqs[i] * 2 * np.pi * x + cmath.phase(fft3[i])))
+        )
+        #print(sinusoid)
+        recomb += sinusoid
+        plt.plot(x, sinusoid)
 
-# Plot the normalized FFT (|Xk|)/(N/2)
-plt.plot(np.abs(fourier)/normalize)
-plt.ylabel('Amplitude')
-plt.xlabel('Samples')
-plt.title('Normalized FFT Spectrum')
+plt.draw()
+plt.figure(3)
+
+plt.plot(x, recomb, x, sin3)
 plt.show()
