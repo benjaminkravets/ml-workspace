355d22bfb6e48c33d5a4e64c41566f8d2c0c6f32
Benjamin Kravets <bakravets@gmail.com>
2023-10-02 12:00:28 -0400

    add parallel arg iteration

--- parallel.py
+++ parallel.py
@@ -1,22 +1,34 @@
-import wolfhaley
+
 import multiprocessing
 import time
 import itertools
 from pprint import pprint
+import sys
 
 
 
 def cube(x):
-    l = 0
+
+    averages = [2.6, 2.54, 1.99]
+
     print(x)
-    #for i in range(x ** 2):
-    #    l += x
-    return(l)
+
+    for i, value in enumerate(x):
+        if value:
+            print(averages[i], end=' ')
+        else:
+            print("na", end=' ')
+    print("\n")
+
 
 if __name__ == "__main__":
     pool = multiprocessing.Pool(4)
 
-    superset = list(itertools.product([0,1], repeat=20))
+    superset = list(itertools.product([0,1], repeat=9))
+    superset = [list(x) for x in superset]
+
+    print(len(superset))
+    sys.exit()
 
     start_time = time.perf_counter()
     processes = [pool.apply_async(cube, args=(x,)) for x in superset]
