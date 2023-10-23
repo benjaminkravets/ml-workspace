d79c146b3e4422b58adb146527ec6bc9e1066180
Benjamin Kravets <bakravets@gmail.com>
2023-10-23 10:48:23 -0400

    append non-df data to a csv-read dataframe

--- test.py
+++ test.py
@@ -1,39 +1,21 @@
 import pandas as pd
 
-def calculate_weighted_average_cv(category_float_pairs):
-  """Calculates the weighted average coefficient of variation (weighted average CV) for a group of category and float pairs.
+df = pd.read_csv('datashop/births.csv')
 
-  Args:
-    category_float_pairs: A list of category and float pairs, where each pair is a tuple of (category, float value).
+import pandas as pd
+import numpy as np
 
-  Returns:
-    The weighted average CV.
-  """
+# Create a list
+new_row = ['1960-1-28', 97]
 
-  # Create a DataFrame from the list of pairs.
-  df = pd.DataFrame(category_float_pairs, columns=["category", "float"])
+# Reshape the new row to match the column names
+new_row = np.reshape(new_row, (1, 2))
 
-  # Calculate the mean float value for each category.
-  mean_float_per_category = df.groupby("category")["float"].mean()
+# Create a new DataFrame from the new row
+new_row_df = pd.DataFrame(new_row, columns=df.columns)
 
-  # Calculate the standard deviation of the float values for each category.
-  std_float_per_category = df.groupby("category")["float"].std()
+# Append the new row to the DataFrame
+df = pd.concat([df, new_row_df], ignore_index=True)
 
-  # Calculate the coefficient of variation (CV) for each category.
-  cv_per_category = std_float_per_category / mean_float_per_category
-
-  # Calculate the number of data points in each category.
-  count_per_category = df.groupby("category")["float"].count()
-
-  # Calculate the weighted average CV.
-  weighted_average_cv = (cv_per_category * count_per_category).sum() / count_per_category.sum()
-
-  return weighted_average_cv
-
-# Example usage:
-
-category_float_pairs = [("A", 1.3), ("B", 2.4), ("B", 2.4), ("C", 3.6), ("C", 3.6)]
-
-weighted_average_cv = calculate_weighted_average_cv(category_float_pairs)
-
-print(weighted_average_cv)
+# Print the DataFrame
+print(df)
